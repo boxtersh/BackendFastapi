814c136c56985fe27a6fa279da03fd526e5445c1
boxtersh <boxtersh@yandex.ru>
2026-04-18 14:31:31 +0300

    rename object todos class Todos - dbase

--- main.py
+++ main.py
@@ -48,32 +48,32 @@ class TodosListResponse(BaseModel):
     todos_list: list[TodoResponse]=[]
 
 app = FastAPI()
-todos = Todos()
+dbase = Todos()
 
 # Создать задачу
 @app.post('/todos', response_model=TodoResponse, status_code=201)
 async def add_todo(todo_data: TodoCreate) -> dict:
-    id_ = todos.increment_id
+    id_ = dbase.increment_id
     todo_response = TodoResponse(
         id = id_,
         title = todo_data.title,
         description = todo_data.description,
         is_completed = todo_data.is_completed
     )
-    todos.todos_dict[id_] = todo_response
+    dbase.todos_dict[id_] = todo_response
     return todo_response.model_dump()
 
 # Получить все задачи
 @app.get('/todos', response_model=TodosListResponse, status_code=200)
 async def get_all_todo_taking_limit(limit: Annotated[Optional[int], Query()] = None) -> dict:
-    list_todos = todos.get_all_todo_taking_limit(limit)
+    list_todos = dbase.get_all_todo_taking_limit(limit)
     todos_list_response = TodosListResponse(todos_list=list_todos)
     return todos_list_response.model_dump()
 
 # Получить задачу по id
 @app.get('/todos/{id_}', response_model=TodoResponse, status_code=200)
 async def get_todo_id(id_: Annotated[int, Path(..., gt=-1)]) -> dict:
-    todo_response = todos.get_todo_id(id_)
+    todo_response = dbase.get_todo_id(id_)
     if not todo_response:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Задача с указанным ID не найдена')
     return todo_response.model_dump()
@@ -81,7 +81,7 @@ async def get_todo_id(id_: Annotated[int, Path(..., gt=-1)]) -> dict:
 # Изменить задачу целиком
 @app.put('/todos/{id_}', response_model=TodoResponse, status_code=200)
 async def put_todo_id(todo_data: TodoCreate, id_: Annotated[int, Path(..., gt=-1)]) -> dict:
-    todo_response = todos.get_todo_id(id_)
+    todo_response = dbase.get_todo_id(id_)
     if not todo_response:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Задача с указанным ID не найдена')
     todo_response.title = todo_data.title
@@ -92,7 +92,7 @@ async def put_todo_id(todo_data: TodoCreate, id_: Annotated[int, Path(..., gt=-1
 # Изменить указанные поля задачи
 @app.patch('/todos/{id_}', response_model=TodoResponse, status_code=200)
 async def patch_todo_id(id_: Annotated[int, Path(..., gt=-1)], update_date: UpdateData=None) -> dict:
-    todo_response = todos.selective_update_date(id_, update_date)
+    todo_response = dbase.selective_update_date(id_, update_date)
     if not todo_response:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Задача с указанным ID не найдена')
     return todo_response.model_dump()
@@ -100,7 +100,7 @@ async def patch_todo_id(id_: Annotated[int, Path(..., gt=-1)], update_date: Upda
 # Удалить задачу по id
 @app.delete('/todos/{id_}', response_model=TodoResponse, status_code=200)
 async def delete_todo_id(id_: Annotated[int, Path(..., gt=-1)]) -> dict:
-    todo_response = todos.del_todo_id(id_)
+    todo_response = dbase.del_todo_id(id_)
     if not todo_response:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Задача с указанным ID не найдена')
     return todo_response.model_dump()
